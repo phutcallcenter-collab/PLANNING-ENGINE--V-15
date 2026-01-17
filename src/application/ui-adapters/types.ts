221ee003e93416582c40a2f8981c8444ce5d2c4c
Juno002 <juniorulloa202@gmail.com>
2026-01-17 10:40:07 -0400

    Delete src directory

--- src/application/ui-adapters/types.ts
+++
@@ -1,7 +0,0 @@
-import { ManagerDuty } from '@/domain/management/types'
-
-export interface EffectiveManagerDay {
-    kind: 'DUTY' | 'VACATION' | 'LICENSE' | 'OFF'
-    duty?: ManagerDuty
-    note?: string
-}
